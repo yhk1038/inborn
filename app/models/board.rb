495dd157e3d990eb8aa87ae0d562f058aea1e9e6
Eric <spb828@naver.com>
2016-07-19 17:07:40 +0900

    board_controller update

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,7 @@
 class Board < ActiveRecord::Base
     has_many :posts
+
+    def self.skip_elts
+      return ['created_at', 'updated_at', 'id']
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
     has_many :replies
     belongs_to :user
     belongs_to :board
+
+    def self.skip_elts
+      return ['created_at', 'updated_at', 'id', 'user_id']
+    end
 end
