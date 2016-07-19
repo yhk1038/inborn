0d9b852a21ee878df4cd2854d7688c7b31abeceb
Eric <spb828@naver.com>
2016-07-20 02:31:36 +0900

    page action

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   # get '/intro(/:tab)', to: 'site#intro'
   # get '/member(/:tab)', to: 'site#member'
-  
+
+  match "/sites(/:id)", to: 'site#page', :via => [:post, :get]
+
   root 'site#entering'
   match "/:controller(/:action(/:id(/:target)))", :via => [:post, :get]
 end
