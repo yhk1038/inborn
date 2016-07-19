452ff48465780bb16396947a91e9280dd0df44c2
Eric <spb828@naver.com>
2016-07-19 21:42:06 +0900

    admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :replies
+  has_many :posts
+
+  def is_admin?
+    true
+  end
+
+  def self.skip_elts
+    ["id", "email", "encrypted_password", "reset_password_token", "reset_password_sent_at", "remember_created_at", "sign_in_count", "current_sign_in_at", "last_sign_in_at", "current_sign_in_ip", "last_sign_in_ip", "created_at", "updated_at"]
+  end
 end
