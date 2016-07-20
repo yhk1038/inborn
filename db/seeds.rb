dd5feccd48b0b9c7de4a3620410e9109991ee34c
yhk1038 <yhk1038@hanmail.net>
2016-07-20 01:11:39 +0000

    6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Intab.create!([
   {tab_id: 1, title: "인사말", description: "법무법인 인본은 사람을 근본으로 생각하며 일하고, 사람이 숨쉬는 자연을 소중히 합니다.", turn: 1, template: "introduce", container: "<img src=\"/main/images/templete/img2_for_introduce.jpg\" style=\" float:right;width:200px;height:200px;margin:0px 5px;\">    <img src=\"/main/images/templete/img1_for_introduce.jpg\" style=\" float:right;width:200px;height:200px;margin:0px 5px;\">    <p id=\"introduce-title\">\"법무법인 인본은 젊은 변호사들의 열의와 열정으로 설립되었습니다.\"</p>    <pre id=\"introduce-content\">법무법인 인본의 변호사들은 모두 일선에서 뛰어난 변호사 업무를 수행하며 그 능력을 인정받았습니다.\n열정과 능력을 겸비한 젊은 40대 변호사들이 보다 훌륭한 소송 진행 및 법률 서비스를 제공하기 위해  한데 모여 법무법인 인본을 설립하였습니다.\n \n법무법인 인본은  사람 '인(人)', 근본 '본(本)'에서 나타나듯이 사람들의 근본적인 도리를 따르고, 그 도리를 법률과 가장 조화될 수 있는 방법을 제시하며, 인간으로서 누릴 수 있는 최고의 행복과 기본적인 권리를 찾을 수 있도록 최선을 다 할 것입니다.\n저희 법무법인 인본과 함께 하신다면 언제나 최고의 서비스를 제공받고, 최상의 결과를 얻을 수 있을 것임을 자신있게 말씀드리는 바입니다.\n \n법무법인 인본은 국내 모든 소송분야(민사, 형사, 가사, 행정, 헌법, 파산 중재 및 조정)를 진행하고 있으며, 특히 다양한 경험과 전문 지식을 바탕으로 관련 분야에서 최고의 법률 서비스를 제공하고 있습니다. 또한 유능한 국제 변호사님과 여러 분야의 자문위원을 통하여 국내외 소송 진행에 있어 효율성을 극대화하고 있습니다.\n \n앞으로도 저희 법무법인 인본 구성원들은 한사람 한사람의 모든 권리와 이익을 중요시하고, 하루하루 더욱 발전하는 모습을 보여주기 위하여 최대한 노력할 것이며 젊음과 지식이 어우러진 최고의 법무법인으로 성장해 나갈 것임을 다짐하는 바입니다.\n \n여러분의 든든한 동반자 법무법인 인본에 대한 아낌없는 성원과 채찍질을 부탁드립니다.\n \n감사합니다.</pre>\n    <p id=\"introduce-signature\">법무법인 인본 임직원 일동</p>"},
   {tab_id: 1, title: "인본의 꿈", description: "법무법인 인본은 사람을 근본으로 생각하며 일하고, 사람이 숨쉬는 자연을 소중히 합니다.", turn: 2, template: "introduce", container: "<img src=\"/main/images/inbon_img.gif\" style=\" float:right;width:200px;height:200px;margin:0px 5px;\">    <p id=\"introduce-title\">\"열 길 물 속은 알아도 사람 속은 모른다.\"</p>    <pre id=\"introduce-content\">많은 소송의뢰인들이 사람에 대한 깊은 배신감과 상처를 안고 마지막 희망의 끈을 가지고\n저희 법무법인 인본을 찾아주시는 모습을 보면서 안타까움과 무거운 책임감을 동시에 느끼며\n인본은  의뢰인의 문제를 함께 공감하고 끈끈한 유대관계를 통해 다각도에서 문제를 해결해드리고자\n노력하고 있습니다.\n \n동시에 현재 각박해진 우리나라 사회 풍토를 보면서 단순히 당장 눈 앞의 이익과 분쟁 해결에만\n그치는 것이 아니라 대한민국과 우리나라 사회 전반에  이바지할 수 있는 법무법인으로서\n다양한 방면에서 활동하고 있습니다.\n \n가까운 곳에서는 인본에서 근무하는 직원들의 근무환경 및 복지 향상에서부터,\n나아가 사회 소외계층 및 약자인 여성인권과 장애인의 기본권 보장 및 봉사, 기부활동에 이르기까지\n다양한 사회봉사활동을 통해 대한민국 사회가 모든 사람이 서로 존중하고 배려하며 더불어 살 수 있는 세상이\n되도록 인본은 항상 희망을 가지고 꿈꾸며 나아가고 있습니다.\n \n항상 사람을 먼저 생각하고 존중하는 사회\n그것이 바로 <b>人本</b>의 희망이자 꿈꾸는 미래입니다.!</pre>"},
-  {tab_id: 1, title: "조직", description: "법무법인 인본은 사람을 근본으로 생각하며 일하고, 사람이 숨쉬는 자연을 소중히 합니다.", turn: 1, template: "org", container: nil},
+  {tab_id: 1, title: "조직", description: "인본의 구성원 조직도", turn: 1, template: "org", container: nil},
   {tab_id: 1, title: "찾아오시는 길", description: "법무법인 인본은 사람을 근본으로 생각하며 일하고, 사람이 숨쉬는 자연을 소중히 합니다.", turn: 1, template: "map", container: nil},
   {tab_id: 1, title: "소셜네트워크", description: "법무법인 인본은 사람을 근본으로 생각하며 일하고, 사람이 숨쉬는 자연을 소중히 합니다.", turn: 1, template: "card", container: nil},
   {tab_id: 2, title: "대표변호사", description: "법무법인 인본은 사람을 근본으로 생각하며 일하고, 사람이 숨쉬는 자연을 소중히 합니다.", turn: 1, template: "people", container: nil},
@@ -28,9 +28,19 @@ Tab.create!([
 ])
 Board.create!([
   {intab_id: 1},
-  {intab_id: 2}
+  {intab_id: 2},
+  {intab_id: 3},
+  {intab_id: 4},
+  {intab_id: 5, title: "인본의 사회적 네트워크"}
 ])
 Post.create!([
   {board_id: 1, title: "\"법무법인 인본은 젊은 변호사들의 열의와 열정으로 설립되었습니다.\"", content: "법무법인 인본의 변호사들은 모두 일선에서 뛰어난 변호사 업무를 수행하며 그 능력을 인정받았습니다.\n열정과 능력을 겸비한 젊은 40대 변호사들이 보다 훌륭한 소송 진행 및 법률 서비스를 제공하기 위해  한데 모여 법무법인 인본을 설립하였습니다.\n\n법무법인 인본은  사람 '인(人)', 근본 '본(本)'에서 나타나듯이 사람들의 근본적인 도리를 따르고, 그 도리를 법률과 가장 조화될 수 있는 방법을 제시하며, 인간으로서 누릴 수 있는 최고의 행복과 기본적인 권리를 찾을 수 있도록 최선을 다 할 것입니다.\n저희 법무법인 인본과 함께 하신다면 언제나 최고의 서비스를 제공받고, 최상의 결과를 얻을 수 있을 것임을 자신있게 말씀드리는 바입니다.\n\n법무법인 인본은 국내 모든 소송분야(민사, 형사, 가사, 행정, 헌법, 파산 중재 및 조정)를 진행하고 있으며, 특히 다양한 경험과 전문 지식을 바탕으로 관련 분야에서 최고의 법률 서비스를 제공하고 있습니다. 또한 유능한 국제 변호사님과 여러 분야의 자문위원을 통하여 국내외 소송 진행에 있어 효율성을 극대화하고 있습니다.\n\n앞으로도 저희 법무법인 인본 구성원들은 한사람 한사람의 모든 권리와 이익을 중요시하고, 하루하루 더욱 발전하는 모습을 보여주기 위하여 최대한 노력할 것이며 젊음과 지식이 어우러진 최고의 법무법인으로 성장해 나갈 것임을 다짐하는 바입니다.\n\n여러분의 든든한 동반자 법무법인 인본에 대한 아낌없는 성원과 채찍질을 부탁드립니다.\n감사합니다.", subtitle: "법무법인 인본 임직원 일동", img1: "/main/images/templete/img1_for_introduce.jpg", img2: "/main/images/templete/img2_for_introduce.jpg"},
-  {board_id: 2, title: "", content: "", img1: "", img2: ""}
+  {board_id: 2, title: "\"열 길 물 속은 알아도 사람 속은 모른다.\"", content: "많은 소송의뢰인들이 사람에 대한 깊은 배신감과 상처를 안고 마지막 희망의 끈을 가지고\n저희 법무법인 인본을 찾아주시는 모습을 보면서 안타까움과 무거운 책임감을 동시에 느끼며\n인본은  의뢰인의 문제를 함께 공감하고 끈끈한 유대관계를 통해 다각도에서 문제를 해결해드리고자\n노력하고 있습니다.\n\n동시에 현재 각박해진 우리나라 사회 풍토를 보면서 단순히 당장 눈 앞의 이익과 분쟁 해결에만\n그치는 것이 아니라 대한민국과 우리나라 사회 전반에  이바지할 수 있는 법무법인으로서\n다양한 방면에서 활동하고 있습니다.\n\n가까운 곳에서는 인본에서 근무하는 직원들의 근무환경 및 복지 향상에서부터,\n나아가 사회 소외계층 및 약자인 여성인권과 장애인의 기본권 보장 및 봉사, 기부활동에 이르기까지\n다양한 사회봉사활동을 통해 대한민국 사회가 모든 사람이 서로 존중하고 배려하며 더불어 살 수 있는 세상이\n되도록 인본은 항상 희망을 가지고 꿈꾸며 나아가고 있습니다.\n\n항상 사람을 먼저 생각하고 존중하는 사회\n그것이 바로 人本의 희망이자 꿈꾸는 미래입니다.!", img1: "/main/images/inbon_img.gif", img2: ""},
+  {board_id: 3, title: "{   인본의 구성원 조직도   }", content: "", img1: "/main/images/templete/organization_structure.jpg", img2: ""},
+  {board_id: 5, title: "나무여성인권상담소", content: "http://www.namuright.or.kr/", img1: "background:#F44336;", img2: ""},
+  {board_id: 5, title: "뉴패러다임인스티튜트", content: "http://www.npikorea.com/xe/", img1: "background:#03A9F4;", img2: ""},
+  {board_id: 5, title: "생명나눔실천본부", content: "http://www.lisa.or.kr/main.do", img1: "background:#8BC34A;", img2: ""},
+  {board_id: 5, title: "대한변호사협회", content: "http://www.koreanbar.or.kr/", img1: "background:#FF9800;", img2: ""},
+  {board_id: 5, title: "자연환경국민신탁", content: "http://www.trustnature.org/", img1: "background:#E91E63;", img2: ""}
+  
 ])
