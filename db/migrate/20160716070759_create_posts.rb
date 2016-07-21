f8a1dde0a13fc512c29be8ec0573c06bb3878007
Eric <spb828@naver.com>
2016-07-21 15:42:25 +0900

    big push

--- db/migrate/20160716070759_create_posts.rb
+++ db/migrate/20160716070759_create_posts.rb
@@ -4,9 +4,12 @@ class CreatePosts < ActiveRecord::Migration
         t.string :title     #제목
         t.string :subtitle  #보조타이틀
         t.text :content     #내용
+        t.string :kind      #종류
         t.string :img1
         t.string :img2
-        
+        t.string :file1
+        t.string :file2
+
         t.integer :user_id  #user
         t.integer :board_id #board
 
