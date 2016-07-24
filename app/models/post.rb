e91d1fd3be4471d445fb451238690a086e5998e0
Eric Park <spb828@naver.com>
2016-07-24 19:00:57 +0900

    m

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,13 @@ class Post < ActiveRecord::Base
     belongs_to :board
     # mount_uploader :img1, ImageUploader
     # mount_uploader :img2, ImageUploader
-    # mount_uploader :file1, ImageUploader
-    # mount_uploader :file2, ImageUploader
+    mount_uploader :file1, ImageUploader
+    mount_uploader :file2, ImageUploader
 
     def self.skip_elts
       return ['created_at', 'updated_at', 'id', 'user_id']
     end
-    
+
     def self.board
       return Board.find(self.board_id)
     end
